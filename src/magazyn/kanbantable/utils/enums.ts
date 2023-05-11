69a19b85d69a891121345f3e0b75b8c698e118d5
Mikolaj <mikus1001@gmail.com>
2023-05-11 18:07:55 +0200

    kanban v1

--- src/magazyn/kanbantable/utils/enums.ts
+++ src/magazyn/kanbantable/utils/enums.ts
@@ -1,8 +1,8 @@
 export enum ColumnType {
-  TO_DO = "Todo",
-  IN_PROGRESS = "In Progress",
-  BLOCKED = "Blocked",
-  COMPLETED = "Completed",
+  TO_DO = "Do zrobienia",
+  IN_PROGRESS = "W trakcie",
+  BLOCKED = "Zablokowane",
+  COMPLETED = "Ukończone",
 }
 
 export enum ItemType {
